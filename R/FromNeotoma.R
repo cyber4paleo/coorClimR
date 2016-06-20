7974b5f1db366252e95c496bd0b5b06cdf0c7eb7
scottsfarley93 <scott@firesphere.org>
2016-06-20 16:29:40 -0600

    Neotoma conversion

--- R/FromNeotoma.R
+++ R/FromNeotoma.R
@@ -1,5 +1,6 @@
 library(jsonlite)
 convertNeotomaPollenToDF <- function(taxonname){
+  ## uses the pollen endpoint with relative abundances
   root = "http://apidev.neotomadb.org/v1/data/pollen?"
   url = paste(root, "taxonname=", taxonname, sep="")
   print(url)
@@ -14,17 +15,24 @@ convertNeotomaPollenToDF <- function(taxonname){
 }
 
 convertNeotomaSDToDF <- function(taxonname){
+  ## uses the sample data endpoint
   root = "http://api.neotomadb.org/v1/data/sampledata?"
   url = paste(root, "taxonname=", taxonname, sep="")
   url = URLencode(url)
   print(url)
   response = fromJSON(url)
-  data <- response$data ## this is teh array
+  data <- response$data ## this is the array
   data['Latitude'] <- (data['SiteLatitudeNorth'] + data['SiteLatitudeSouth'])/2
   data['Longitude'] <- (data['SiteLongitudeEast'] + data['SiteLongitudeWest']) / 2
-  fields <- c("DatasetID","SampleAge", "Latitude", "Longitude")
-  names(df) <- c("ID", "Age", "Latitude", "Longitude")
+  interpAge <- (data['SampleAgeYounger'] + data['SampleAgeOlder']) / 2
+  data[which(is.na(data['SampleAge'])), ]['SampleAge'] <- interpAge
+  fields <- c("SampleAge", "Latitude", "Longitude")
   df <- data[fields]
+  names(df) <- c("Age", "Latitude", "Longitude")
+  df <- na.omit(df)
+  df <- unique(df)
   return(df)
 }
+
+
 
